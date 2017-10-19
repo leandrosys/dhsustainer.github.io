4f5332305cabffe8eccb5fd3388580c17ab153bc
Daniel Herrera <admin@digammastudio.com>
2017-10-19 18:52:33 -0500

    Portfolio CSS, JS, HTML

---
+++ js/script.js
@@ -0,0 +1,11 @@
+(function($){
+	$('.nav-portfolio ul li a').click(function(event) {
+		/* Act on the event */
+		event.preventDefault();
+		var item;
+
+		item = $(this).data('class');
+		$('.item').hide();
+		$(item).fadeIn();
+	});
+})(jQuery)
