c93c8cce0c47e19dc7624faacaa19e545c5c6526
Daniel Herrera <admin@digammastudio.com>
2017-10-24 17:30:56 -0500

    Instagram BTN

--- js/api.js
+++ js/api.js
@@ -3,13 +3,13 @@ https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN
 	$('#instagram-btn').click(function(event) {
 		/* Act on the event */
 		event.preventDefault();
-		var access_token = '1362124742.3ad74ca.6df307b8ac184c2d830f6bd7c2ac5644';
+		var token = '1362124742.3ad74ca.6df307b8ac184c2d830f6bd7c2ac5644';
 
 		$.ajax({
 			url: 'https://api.instagram.com/v1/users/self/',
 			type: 'GET',
 		    dataType: 'JSONP',
-			data: {access_token:access_token},
+			data: {access_token:token},
 			success: function(data) {
 				console.log(data.caption.text);
 			},
